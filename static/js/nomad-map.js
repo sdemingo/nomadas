3bdf7690cd9904228683ac9b3ed1950cd325d446
Sergio de Mingo <sdemingo@gmail.com>
2016-01-25 19:42:07 +0100

    rebuild main ui with new JS code

---
+++ static/js/nomad-map.js
@@ -0,0 +1,30 @@
+
+
+
+var nomadmap = (function(){
+
+    var bindFunctions = function(){
+
+    }
+
+    var init = function(){
+	bindFunctions()
+
+	var initLoc = new google.maps.LatLng(40.4279613,0.2967822)
+
+	var mapOptions = {
+	    zoom: 6,
+	    center: {lat: -33, lng: 151},
+	    disableDefaultUI: true,
+	    center:initLoc
+	}
+
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions)
+    }
+
+    return{
+	init:init
+    }
+
+
+})()
