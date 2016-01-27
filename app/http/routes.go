ca3dc5ffbaab48c576869d347e5b8cc97c5b17da
Sergio de Mingo <sdemingo@gmail.com>
2016-01-27 17:03:00 +0100

    change app core files

--- app/http/routes.go
+++ app/http/routes.go
@@ -2,10 +2,20 @@ package http
 
 import (
 	"net/http"
+
+	"model/users"
 )
 
 func init() {
 	http.HandleFunc("/help", func(w http.ResponseWriter, r *http.Request) {
 		AppHandler(w, r, Help)
+	})
+
+	// Users routes
+	http.HandleFunc("/users/me", func(w http.ResponseWriter, r *http.Request) {
+		AppHandler(w, r, users.GetOne)
+	})
+	http.HandleFunc("/users/add", func(w http.ResponseWriter, r *http.Request) {
+		AppHandler(w, r, users.Add)
 	})
 }
