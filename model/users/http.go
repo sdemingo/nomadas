9cf232685cf3b253f80ddfec4dea9dfe64a320f9
Sergio de Mingo <sdemingo@gmail.com>
2016-01-27 17:02:39 +0100

    cleaning users handlers

--- model/users/http.go
+++ model/users/http.go
@@ -3,6 +3,7 @@ package users
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"appengine/srv"
@@ -36,33 +37,38 @@ func GetList(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
 }
 
 func GetOne(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
+	var id int64
 	var err error
-	var nus []*NUser
 
+	wr.R.ParseForm()
 	if strings.HasSuffix(wr.R.URL.Path, "/me") {
 		if wr.NU.ID() > 0 {
-			filters := map[string][]string{"id": []string{fmt.Sprintf("%d", wr.NU.ID())}}
-			nus, err := getUsers(wr, filters)
+			id = wr.NU.ID()
+			nu, err := getUserById(wr, id)
 			if err != nil {
 				return viewTmpl, fmt.Errorf("users: getone: %v", err)
 			}
-			tc["Content"] = nus[0]
+			tc["Content"] = nu
 		} else {
 			tc["Content"] = wr.NU
 		}
-
 	} else {
 		if wr.NU.GetRole() < ROLE_ADMIN {
-			return viewTmpl, fmt.Errorf("users: getone: %s", ERR_NOTOPERATIONALLOWED)
+			return infoTmpl, fmt.Errorf("users: getone: %s", ERR_NOTOPERATIONALLOWED)
 		}
 
-		wr.R.ParseForm()
-		nus, err = getUsers(wr, wr.R.Form)
+		id, err = strconv.ParseInt(wr.R.Form["id"][0], 10, 64)
+		if err != nil {
+			return infoTmpl, fmt.Errorf("%v: %s", err, ERR_USERNOTFOUND)
+		}
+
+		nu, err := getUserById(wr, id)
 		if err != nil {
 			return viewTmpl, fmt.Errorf("users: getone: %v", err)
 		}
-		tc["Content"] = nus[0]
+
 		tc["UserProfile"] = true
+		tc["Content"] = nu
 	}
 
 	return viewTmpl, nil
@@ -71,65 +77,6 @@ func GetOne(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
 func New(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
 	tc["ImportForm"] = true
 	return newTmpl, nil
-}
-
-func Edit(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
-	if wr.NU.GetRole() < ROLE_ADMIN {
-		return viewTmpl, fmt.Errorf("users: edit: %s", ERR_NOTOPERATIONALLOWED)
-	}
-
-	wr.R.ParseForm()
-	nus, err := getUsers(wr, wr.R.Form)
-	if err != nil {
-		return viewTmpl, fmt.Errorf("users: edit: %v", err)
-	}
-
-	tc["Content"] = nus[0]
-
-	return newTmpl, nil
-}
-
-func Delete(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
-	if wr.NU.GetRole() < ROLE_ADMIN {
-		return viewTmpl, fmt.Errorf("users: delete: %s", ERR_NOTOPERATIONALLOWED)
-	}
-
-	wr.R.ParseForm()
-	nus, err := getUsers(wr, wr.R.Form)
-	if err != nil {
-		return viewTmpl, fmt.Errorf("users: delete: %v", err)
-	}
-	err = deleteUser(wr, nus[0])
-	if err != nil {
-		return viewTmpl, fmt.Errorf("users: delete: %v", err)
-	}
-
-	tc["Content"] = "Usuario borrado con éxito"
-
-	return infoTmpl, nil
-}
-
-func Update(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
-	if wr.NU.GetRole() < ROLE_ADMIN {
-		return viewTmpl, fmt.Errorf("users: update: %s", ERR_NOTOPERATIONALLOWED)
-	}
-
-	nu := new(NUser)
-
-	decoder := json.NewDecoder(wr.R.Body)
-	err := decoder.Decode(nu)
-	if err != nil {
-		return infoTmpl, fmt.Errorf("users: update: %v", err)
-	}
-
-	err = updateUser(wr, nu)
-	if err != nil {
-		return infoTmpl, fmt.Errorf("users: update: %v", err)
-	}
-
-	tc["Content"] = nu
-
-	return infoTmpl, nil
 }
 
 func Add(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
@@ -151,38 +98,6 @@ func Add(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
 	}
 
 	tc["Content"] = nu
-
-	return infoTmpl, nil
-}
-
-func Import(wr srv.WrapperRequest, tc map[string]interface{}) (string, error) {
-	if wr.NU.GetRole() < ROLE_ADMIN {
-		return viewTmpl, fmt.Errorf("users: import: %s", ERR_NOTOPERATIONALLOWED)
-	}
-
-	file, _, err := wr.R.FormFile("importFile")
-	if err != nil {
-		return infoTmpl, fmt.Errorf("users: import: %v", err)
-	}
-
-	nus := NewNUserBuffer()
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&nus)
-	if err != nil {
-		return infoTmpl, fmt.Errorf("users: import: %v", err)
-	}
-
-	// TODO:
-	// Check if the users are valid
-
-	for _, nu := range nus {
-		err = putUser(wr, nu)
-		if err != nil {
-			return infoTmpl, fmt.Errorf("users: import: %v", err)
-		}
-	}
-
-	tc["Content"] = fmt.Sprintf("Se han creado en la base de datos %d usuarios", len(nus))
 
 	return infoTmpl, nil
 }
