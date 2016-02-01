8b69eb2f743d36ba6c26ba323ae2c5e8e06049aa
Sergio de Mingo <sdemingo@gmail.com>
2016-02-01 17:37:16 +0100

    wrap blobstore methods in appengine/srv

--- appengine/srv/server.go
+++ appengine/srv/server.go
@@ -2,30 +2,46 @@ package srv
 
 import (
 	"net/http"
+	"net/url"
 
 	"app/users"
 
 	"appengine"
+	"appengine/blobstore"
 	"appengine/user"
 )
 
 type WrapperRequest struct {
-	R  *http.Request
-	C  appengine.Context
-	U  *user.User
-	NU users.AppUser
-	//NU           *users.NUser
+	R          *http.Request
+	C          appengine.Context
+	U          *user.User
+	NU         users.AppUser
+	MIMEChunks map[string][]*blobstore.BlobInfo
 
 	JsonResponse bool
 }
 
 func NewWrapperRequest(r *http.Request) WrapperRequest {
 	c := appengine.NewContext(r)
-	return WrapperRequest{r, c, user.Current(c), nil, false}
+	return WrapperRequest{r, c, user.Current(c), nil, nil, false}
 }
 
 func (wr WrapperRequest) IsAdminRequest() bool {
 	return user.IsAdmin(wr.C)
+}
+
+func (wr WrapperRequest) ParseMIMEChunks() error {
+	var err error
+	wr.MIMEChunks, _, err = blobstore.ParseUpload(wr.R)
+	return err
+}
+
+// Return a valid handler to receive the MIME HTTP request. It's be used
+// once as the appengine blobstore model require.
+func (wr WrapperRequest) GetMIMEHandler(baseURL string) (*url.URL, error) {
+	uploadURL, err := blobstore.UploadURL(wr.C, baseURL, nil)
+	return uploadURL, err
+
 }
 
 func Log(wr WrapperRequest, msg string) {
